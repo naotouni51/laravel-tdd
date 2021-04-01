5cb1bf764a4fead4184481ce8aaed3d68db634e9
naotouni51 <naotouni51@gmail.com>
2021-04-01 13:51:08 +0900

    LessonControllerTestの通過とリファクタリング

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -3,14 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Lesson;
-use App\Models\VacancyLevel;
 use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
     public function show(Lesson $lesson)
     {
-        $vacancyLevel = new VacancyLevel(0);
-        return view('lesson.show', compact('lesson', 'vacancyLevel'));
+        return view('lesson.show', compact('lesson'));
     }
 }
