c597e7fbb243c8b18b9ea237201764a71e39fceb
naotouni51 <naotouni51@gmail.com>
2021-04-01 12:42:50 +0900

    テストを通すためのVacancyLevelクラスを作成

---
+++ app/Models/VacancyLevel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VacancyLevel extends Model
+{
+    private $remainingCount;
+
+    public function __construct(int $remainingCount)
+    {
+        $this->remainingCount = $remainingCount;
+    }
+
+    public function mark(): string
+    {
+        if ($this->remainingCount === 0) {
+            return '×';
+        }
+        if ($this->remainingCount === 4) {
+            return '△';
+        }
+        if ($this->remainingCount === 5) {
+            return '◎';
+        }
+    }
+}
